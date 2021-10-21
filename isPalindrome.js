2129e2d2af952e58e1de126d104365026b10f04d
GulenYilmaz <gulenaltintasyilmaz@gmail.com>
2021-10-20 20:40:11 -0400

    isPalindrome

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,1 +1,36 @@
 // Write your code below
+// Return true if this word is a palindrome. false if it is not. A palindrome is a word that is spelled the same backwards and forwards.
+
+// For example:
+
+// isPalindrom("a")
+// // returns true
+
+// isPalindrom("noon")
+// // returns true
+
+// isPalindrom("hello")
+// // returns false
+// Treat spaces and uppercase letters normally—so “Racecar” wouldn’t be a palindrome since “R” and “r” aren’t the same letter:
+
+// isPalindrom("Racecar")
+// // returns false
+
+// isPalindrom("racecar")
+// // returns true
+ 
+
+function checkPalindrom(palindrom)
+{
+
+    for( var i = palindrom.length; i > 0; i-- )
+    {
+        if( palindrom[i] = palindrom.charAt(palindrom.length)-1 )
+        {
+            console.log('the word is palindrome.');
+        }else{
+            console.log('the word is not palindrome!');
+        }
+    }
+}
+checkPalindrom('roor');
